0275bd424e36441291539cee375acd0c0f55acf8
Max Shenfield <shenfieldmax@gmail.com>
2015-06-13 08:53:16 -0500

    Added usernames and load history on connection

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Holds the history of the chat, so
+// anyone joining can see the whole thing
+var history = [];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+
+	socket.emit('welcome', history);
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    history.push(msg);
   });
 });
 
